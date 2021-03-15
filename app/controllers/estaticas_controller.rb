e599c0fe303a9af50ccc0593a336bc29d8158d5c
Miryam Bathilde <mbathilde@gmail.com>
2021-03-15 18:33:58 +0100

    añadiendo dinamismo con provide and content_for

--- app/controllers/estaticas_controller.rb
+++ app/controllers/estaticas_controller.rb
@@ -1,7 +1,13 @@
 class EstaticasController < ApplicationController
   def contacto
+    @id = params[:id]
+    @pag = params[:pagina]
   end
 
   def nosotros
+    @mensaje = "Bienvenidos a tod@s de nuevo a mi blog!"
+    @usuarios = ['Miryam', 'Sergio', 'Oliver', "Luca"]
   end
+
 end
+
