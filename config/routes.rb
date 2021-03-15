4693391d9f142a0f0034d8213c454bb8154c6373
Miryam Bathilde <mbathilde@gmail.com>
2021-03-15 13:16:17 +0100

    añadiendo paginas estaticas, rutas y vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'estaticas/contacto'
+  get 'estaticas/nosotros'
+  
+  get 'home/index'
+  #aqui le estamos indicando que la raiz es el controler home y la vista index
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
